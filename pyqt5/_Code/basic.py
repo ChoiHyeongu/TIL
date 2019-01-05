12840c1cb23aebe1242608db4847db3b04c870cc
ChoiHyeongu <a11dsd123@gmail.com>
2019-01-05 21:18:38 +0900

    pyqt5

--- pyqt5/_Code/basic.py
+++ pyqt5/_Code/basic.py
@@ -13,6 +13,7 @@ class MainDialog(QDialog):
         QDialog.__init__(self, None)
         uic.loadUi(ui_calculator, self)
 
+        # NumberPad
         self.main_btn_0.clicked.connect(lambda state, button=self.main_btn_0: self.numClicked(state, button))
         self.main_btn_1.clicked.connect(lambda state, button=self.main_btn_1: self.numClicked(state, button))
         self.main_btn_2.clicked.connect(lambda state, button=self.main_btn_2: self.numClicked(state, button))
@@ -24,22 +25,48 @@ class MainDialog(QDialog):
         self.main_btn_8.clicked.connect(lambda state, button=self.main_btn_8: self.numClicked(state, button))
         self.main_btn_9.clicked.connect(lambda state, button=self.main_btn_9: self.numClicked(state, button))
 
+        # Calculate Function
         self.main_btn_plus.clicked.connect(lambda state, button=self.main_btn_plus: self.numClicked(state, button))
         self.main_btn_minus.clicked.connect(lambda state, button=self.main_btn_minus: self.numClicked(state, button))
         self.main_btn_multiply.clicked.connect(lambda state, button=self.main_btn_multiply: self.numClicked(state, button))
         self.main_btn_divide.clicked.connect(lambda state, button=self.main_btn_divide: self.numClicked(state, button))
 
+        self.main_btn_bracket1.clicked.connect(lambda state, button=self.main_btn_bracket1: self.numClicked(state, button))
+        self.main_btn_bracket2.clicked.connect(lambda state, button=self.main_btn_bracket2: self.numClicked(state, button))
+        self.main_btn_point.clicked.connect(lambda state, button=self.main_btn_point: self.numClicked(state, button))
+
+        self.main_btn_percentage.clicked.connect(self.percentage)
         self.main_btn_result.clicked.connect(self.makeResult)
+        self.main_btn_reset.clicked.connect(self.reset)
+        self.main_btn_del.clicked.connect(self.delete)
 
     def numClicked(self, state, button):
+
         exist_line_text = self.main_line_input.text()
         now_text_num = button.text()
         self.main_line_input.setText(exist_line_text + now_text_num)
 
     def makeResult(self):
-        result = eval(self.main_line_input.text())
-        print(type(result))
-        self.main_line_output.setText(str(result))
+
+        try:
+            result = eval(self.main_line_input.text())
+            self.main_line_output.setText(str(result))
+        except Exception as e:
+            print(e)
+            pass
+
+    def reset(self):
+        self.main_line_input.clear()
+        self.main_line_output.setText('0')
+
+    def delete(self):
+        exist_line_text = self.main_line_input.text()
+        self.main_line_input.setText(exist_line_text[:-1])
+
+    def percentage(self):
+        val = float(self.main_line_input.text())
+        val = val * 0.01
+        self.main_line_output.setText(str(val))
 
 app = QApplication(sys.argv)
 main_dialog = MainDialog()
