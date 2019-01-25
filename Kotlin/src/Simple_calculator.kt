2e95da74f345460f2a479af9873efb30852c7faf
ChoiHyeongu <a11dsd123@gmail.com>
2019-01-25 23:30:08 +0900

    Kotlin simple calculator

--- Kotlin/src/Simple_calculator.kt
+++ Kotlin/src/Simple_calculator.kt
@@ -1,14 +1,27 @@
-fun main(args: Array<String>) {
-    var x = 3
-    var y = 2
+import java.util.*
 
-    println(sum(x, y))
-    println(minus(x, y))
-    println(multiply(x, y))
-    println(divide(x, y))
+fun main(args: Array<String>) {
+
+    var result = 0
+    val sc: Scanner = Scanner(System.`in`)
+    println("x값을 입력하세요")
+    var x = sc.nextLine()
+    println("y값을 입력하세요")
+    var y = sc.nextLine()
+    println("연산자를 입력하세요")
+    var opt = sc.nextLine()
+
+    when (opt){
+        "+" -> result = sum(x.toInt(), y.toInt())
+        "-" -> result = minus(x.toInt(), y.toInt())
+        "*" -> result = multiply(x.toInt(), y.toInt())
+        "/" -> result = divide(x.toInt(), y.toInt())
+    }
+
+    println("결과 : $result")
 }
 
 fun sum(a: Int, b: Int) = a + b
-fun minus(a: Int, b: Int) =  a - b
-fun multiply(a: Int, b: Int) =  a * b
+fun minus(a: Int, b: Int) = a - b
+fun multiply(a: Int, b: Int) = a * b
 fun divide(a: Int, b: Int) = a / b
