f2ebaf8c84b30eaf36e24b473c9f14ef0ce9a05c
ChoiHyeongu <a11dsd123@gmail.com>
2019-04-01 23:30:36 +0900

    TIL

--- Java/Codeup/src/codeup_4013.java
+++ Java/Codeup/src/codeup_4013.java
@@ -3,19 +3,42 @@ import java.util.Scanner;
 public class codeup_4013 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int[] binary = new int[1000];
-		
+		int[] arr = new int[1000];
+
 		int n = scanner.nextInt();
-		int quotient = 0;
+		int cal_n = n ;
 		int remainder = 0;
 		int cnt = 0;
+
+		while (cal_n >= 2) {
+			remainder = cal_n % 2;
+			cal_n = cal_n / 2;
+			arr[cnt++] = remainder;
+		}
+
+		arr[cnt++] = cal_n;
 		
-		while(n >= 2) {
-			n = n/2;
-			remainder = n%2;
-			System.out.println(remainder);
+		for (int i = cnt-1; i >= 0; i--) {
+			System.out.print(arr[i]);
 		}
 		
+		System.out.println();
 		
+		
+		cal_n = n;
+		cnt = 0;
+
+		while (cal_n >= 8) {
+			remainder = cal_n % 8;x
+			cal_n = cal_n / 8;
+			arr[cnt++] = remainder;
+		}
+		
+		arr[cnt++] = remainder;
+
+		for (int i = cnt-1; i >= 0; i--) {
+			System.out.print(arr[i]);
+		}
+
 	}
 }
---
+++ Java/Test/src/rock_scissors_paper_190331.java
@@ -0,0 +1,80 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class rock_scissors_paper_190331 {
+
+	public static void main(String[] args) {
+
+		final int ROCK = 1;
+		final int SCISSORS = 2;
+		final int PAPER = 3;
+
+		Random random = new Random();
+		Scanner scanner = new Scanner(System.in);
+
+		int yourWinCount = 0, comWinCount = 0;
+		int yourInput, comInput;
+		String result = "";
+
+		System.out.println("즐거운 가위! 바위! 보! 게임~");
+		System.out.println("설명 : 사용자는 가위바위보를 낼 수 있습니다.");
+		System.out.println("1은 가위, 2는 바위, 3은 보!!");
+		System.out.println("그럼 10판을 먼저 이긴쪽이 승리입니다~");
+
+		while (true) {
+			comInput = random.nextInt(3) + 1;
+			System.out.print("용자여 선택하세요~(1.가위/2.바위/3.보)>>");
+			yourInput = scanner.nextInt();
+
+			if (yourInput < 0 || yourInput > 3) {
+				System.out.println("ㅡㅡ;; 1~3번만 입력하라니깐요.");
+				continue;
+			}
+
+			if (yourInput == ROCK) {
+				if (comInput == SCISSORS) {
+					result = "Your WIN ";
+					yourWinCount++;
+				} else if (comInput == PAPER) {
+					result = "Your LOSE";
+					comWinCount++;
+				} else {
+					result = "Draw Game";
+				}
+			} else if (yourInput == SCISSORS) {
+				if (comInput == PAPER) {
+					result = "Your WIN ";
+					yourWinCount++;
+				} else if (comInput == ROCK) {
+					result = "Your LOSE";
+					comWinCount++;
+				} else {
+					result = "Draw Game";
+				}
+			} else // yourInput == PAPER{
+			if (comInput == ROCK) {
+				result = "Your WIN ";
+				yourWinCount++;
+			} else if (comInput == SCISSORS) {
+				result = "Your LOSE";
+				comWinCount++;
+			} else {
+				result = "Draw Game";
+			}
+
+			System.out.println("┌─────────────────────────────┐");
+			System.out.println("│          " + result + "          │");
+			System.out.println("├──────┬──────────────────────┤");
+			System.out.printf("│RESULT│ User:%2dWIN, COM:%2dWIN│\n", yourWinCount, comWinCount);
+			System.out.println("└──────┴──────────────────────┘");
+
+			if (yourWinCount >= 10) {
+				System.out.println("사용자의 압승으로 게임에서 최종 우승하였습니다.");
+				break;
+			} else if (comWinCount >= 10) {
+				System.out.println("다음에 또 다시 도전하세요ㅜㅜ...");
+				break;
+			}
+		}
+	}
+}
