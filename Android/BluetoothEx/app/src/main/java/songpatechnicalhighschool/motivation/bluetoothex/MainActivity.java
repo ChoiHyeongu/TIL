487c44a8a43aaef6dbd5830f066b7ccbfadafd84
ChoiHyeongu <a11dsd123@gmail.com>
2019-05-28 23:31:34 +0900

    TIL

--- Android/BluetoothEx/app/src/main/java/songpatechnicalhighschool/motivation/bluetoothex/MainActivity.java
+++ Android/BluetoothEx/app/src/main/java/songpatechnicalhighschool/motivation/bluetoothex/MainActivity.java
@@ -6,18 +6,23 @@ import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -28,54 +33,113 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    String TAG = "Bluetooth";
-
-    RecyclerView bluetoothList;
-    ArrayList<String> bluetoothArray = new ArrayList<String>();
-    BluetoothListAdapter bluetoothListAdapter;
-    BluetoothAdapter bluetoothAdapter;
+    private BluetoothAdapter mBtAdapter;
+    private ListView mLvDevices;
+    private ArrayList<String> mDeviceList = new ArrayList<String>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(bluetoothAdapter == null)
-            Toast.makeText(this, "블루투스를 지원하지 않습니다.", Toast.LENGTH_SHORT).show();
-
-        if(!bluetoothAdapter.isEnabled()){
-            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBT, 1);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkVerify();
+        } else {
+            startApp();
         }
 
-        findPairedBluetooth();
-        findNewBluetooth();
+        mLvDevices = (ListView) findViewById(R.id.lvDevices);
+
 
     }
 
-    void findPairedBluetooth(){
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        if (pairedDevices.size() > 0) {
-            for (BluetoothDevice device : pairedDevices) {
-                Toast.makeText(this, device.getName(), Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    void findNewBluetooth(){
-        Toast.makeText(this, "find new device", Toast.LENGTH_SHORT).show();
-        final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-                Log.d("New Device", "ACtion");
-                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    Log.d("New Device", device.getName());
-                }
-            }
-        };
+    void startApp(){
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver, filter);
+        registerReceiver(mBtReceiver, filter);
+
+        // Getting the Bluetooth adapter
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBtAdapter != null) {
+            mBtAdapter.startDiscovery();
+            Toast.makeText(this, "Starting discovery...", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Bluetooth disabled or not available.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mBtAdapter != null) {
+            mBtAdapter.cancelDiscovery();
+        }
+        unregisterReceiver(mBtReceiver);
+    }
+
+    private final BroadcastReceiver mBtReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mDeviceList.add(device.getAddress() + ", " + device.getName()); // get mac address
+                Toast.makeText(context, device.getName(), Toast.LENGTH_SHORT).show();
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, mDeviceList);
+                mLvDevices.setAdapter(adapter);
+            }
+        }
+    };
+
+    public void checkVerify() {
+        if (checkSelfPermission(Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (shouldShowRequestPermissionRationale(Manifest.permission.BLUETOOTH)) {
+
+            }
+            requestPermissions(new String[]{Manifest.permission.BLUETOOTH,
+                            Manifest.permission.BLUETOOTH_ADMIN},
+                    1);
+        }
+        else{
+            startApp();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1)
+        {
+            if (grantResults.length > 0)
+            {
+                for (int i=0; i<grantResults.length; ++i)
+                {
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED)
+                    {
+                        // 하나라도 거부한다면.
+                        new AlertDialog.Builder(this).setTitle("알림").setMessage("권한을 허용해주셔야 앱을 이용할 수 있습니다.")
+                                .setPositiveButton("종료", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        finish();
+                                    }
+                                }).setNegativeButton("권한 설정", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                        .setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                                getApplicationContext().startActivity(intent);
+                            }
+                        }).setCancelable(false).show();
+
+                        return;
+                    }
+                }
+                startApp();
+            }
+        }
     }
 }
