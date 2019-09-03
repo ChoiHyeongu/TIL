edbc4b4e71c018aefe5163c47d46e3d826527c93
ChoiHyeongu <a11dsd123@gmail.com>
2019-09-03 23:12:33 +0900

    Remove Files

---
+++ Android/GithubAPIWithRetrofit/app/src/main/java/com/example/githubapiwithretrofit/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.githubapiwithretrofit.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.githubapiwithretrofit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
