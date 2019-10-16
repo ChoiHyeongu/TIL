cb73a9c05e89e4e987ac2c0ddbafa06eb608a915
ChoiHyeongu <a11dsd123@gmail.com>
2019-10-16 23:55:47 +0900

    TIL

--- Java/WearherParser/src/Main.java
+++ Java/WearherParser/src/Main.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		
-		Weather weather = new Weather();
+		int input = 1;
 		
-		weather.getWeather(29);
+		Weather weather = new Weather();
+		weather.getCalender();
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(input!=0) {
+			System.out.println();
+			System.out.println("오늘을 기준으로 앞으로 7일간의 날씨만 알 수 있습니다.");
+			System.out.print("날짜를 선택하세요 : ");
+			input = scanner.nextInt();
+			weather.getWeather(input);
+		}
 	}
 }
--- Java/WearherParser/src/Weather.java
+++ Java/WearherParser/src/Weather.java
@@ -1,33 +1,84 @@
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Calendar;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
-public class Weather{
-	
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Weather {
+
 	public void getWeather(int date) {
-		
-		char[] arr = new char[100];
-		
-		final String url = "https://weather.com/ko-KR/weather/monthly/l/c86f97bf8a27ec300b062be90c114b04aa9edb4c4d5a77ceeae44f234210a622";
-		String result = null;
-		
+
 		try {
-			Document html = Jsoup.connect(url).get();
-			Elements table = html.getElementsByClass("dayCell opaque");
-			String dateList = table.toString();
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+			Document document = documentBuilder
+					.parse("http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=109");
+
+			document.getDocumentElement().normalize();
+			System.out.println("Root element: " + document.getDocumentElement().getNodeName());
+
+			NodeList nList = document.getElementsByTagName("tmEf");
 			
-			arr = dateList.toCharArray();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			for(int i = 0 ; i < nList.getLength(); i++) {
+				String tmEf = nList.getNodeValue();
+			}
+
+			System.out.println("파싱할 리스트 수 : " + nList.getLength()); // 파싱할 리스트 수
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		for(int i=0;i<965;i++) {
-			System.out.print(arr[i]);
+
+		System.out.println();
+		System.out.println("[17일은 21도에 날씨는 맑음입니다.]");
+		System.out.println();
+	}
+
+	public void getCalender() {
+
+		Calendar cal = Calendar.getInstance(); // 캘린더 인스턴스 얻기
+
+		int today = cal.get(Calendar.DATE); // 오늘 일자 저장
+
+		int year = 2019;
+		int month = 10;
+
+		// 원하는 년도 날짜 세팅
+		cal.set(Calendar.YEAR, year);
+		cal.set(Calendar.MONTH, month - 1);
+
+		cal.set(Calendar.DATE, 1); // 현재 달을 1일로 설정.
+		int sDayNum = cal.get(Calendar.DAY_OF_WEEK); // 1일의 요일 얻어오기, SUNDAY (1) .MONDAY(2) , TUESDAY(3),.....
+		int endDate = cal.getActualMaximum(Calendar.DATE); // 달의 마지막일 얻기
+
+		int nowYear = cal.get(Calendar.YEAR);
+		int nowMonth = cal.get(Calendar.MONTH);
+
+		System.out.println("===================== " + nowYear + "년  " + (nowMonth + 1) + "월 ==================");
+
+		int sDay = sDayNum;
+
+		// 요일명 출력
+		System.out.println(" 일\t 월\t 화\t 수\t 목\t 금\t 토\t");
+		System.out.println("====================================================");
+		// 1일이 시작되는 이전의 요일 공백으로 채우기
+		for (int i = 1; i < sDayNum; i++) {
+			System.out.printf("%3s\t", "★");
+		} // for-----------------
+		for (int i = 1; i <= endDate; i++) { // 1부터 마지막일까지 반복.
+
+			if (i == today)
+				System.out.printf("[%2d]\t", i); // 오늘 날짜 표시
+			else
+				System.out.printf("%3d\t", i); // 일반 출력
+
+			if (sDay % 7 == 0)
+				System.out.println(); // 요일이 7배수가 되면 줄바꿈.
+			sDay++; // 요일 카운트
 		}
 	}
 }
