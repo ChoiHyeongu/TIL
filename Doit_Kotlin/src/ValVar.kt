b3efa4ad29df508ca5d3e71f0d74db219f369117
ChoiHyeongu <a11dsd123@gmail.com>
2019-11-17 21:32:49 +0900

    Do it Kotlin Chapter2

---
+++ Doit_Kotlin/src/ValVar.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val number = 10
+    var language = "Korean"
+    val secondNumber: Int = 20
+    language = "English"
+
+    println("number: $number")
+    println("language: $language")
+    println("secondNumber: $secondNumber")
+}
